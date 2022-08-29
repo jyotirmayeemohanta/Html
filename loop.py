f0c0278faf96a63d677069f60c392b2eaa7bf0ea
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-08-29 22:10:17 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,55 @@
+# r=5
+# while r>=1:
+#     c=1
+#     while c<=r:
+#         print("*",end="")
+#         c+=1
+#     print()
+#     r-=1
+
+
+# s="Laptop"
+# print(s[::-1])
+
+# i=1
+# while i<=20:
+#     if i%3==0:
+#         print("fizz")
+#     elif i%5==0:
+#         print("buzz")
+#     elif i%3==0 and i%5==0:
+#         print("fizz buzz")
+#     else:
+#         print(i)
+#     i+=1
+
+# i=1
+# while i<=20:
+#     if i%3==0:
+#         print("fizz")
+#     elif i%5==0:
+#         print("buzz")
+#     elif i%3==0 and i%5==0:
+#         print("fizz buzz")
+#     else:
+#         print(i)
+#     i+=1
+
+
+n=int(input("enter a no"))
+a=0
+b=1
+c=a+b
+while c<=n:
+    print(c)
+    a=b
+    b=c
+    c=a+b
+    
+
+ 
+
+
+
+
+
